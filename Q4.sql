9b6c61157ce124831cca3efefd3de275960bb751
ronin62715sv <slateralexander219@yahoo.com>
2024-10-07 16:22:46 -0400

    Fixed query for Q4.

--- Q4.sql
+++ Q4.sql
@@ -1,15 +1,15 @@
 --Query for Question 4
 
-WITH t1\
+WITH t1
   AS (SELECT DATE_TRUNC('day', r.rental_date) AS day,
-             COUNT(r.rental_id) rental_count FROM rental 
-        JOIN inventory i\
-        ON i.inventory_id = r.inventory_id\
-        JOIN film f\
-        ON f.film_id = i.film_id\
-        JOIN film_category fc\
-        ON fc.film_id = f.film_id\
-        JOIN category c\
+             COUNT(r.rental_id) rental_count FROM rental r 
+        JOIN inventory i
+        ON i.inventory_id = r.inventory_id
+        JOIN film f
+        ON f.film_id = i.film_id
+        JOIN film_category fc
+        ON fc.film_id = f.film_id
+        JOIN category c
         ON c.category_id = fc.category_id WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
        GROUP BY 1
        ORDER BY 1)
