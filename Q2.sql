316d98eef6577708e98f5a8df6e450d8221fa9ae
ronin62715sv <slateralexander219@yahoo.com>
2024-10-08 15:38:00 -0400

    Refactored query for question 2 to sort sum DESC.

--- Q2.sql
+++ Q2.sql
@@ -1,12 +1,12 @@
 --Query for Question 2
 
-SELECT f.title film_title,
-       c.name category_name,
-       f.rental_duration rental_duration,
-       NTILE(4) OVER (ORDER BY rental_duration) AS standard_quartile
+SELECT c.name category_name,
+       SUM(f.rental_duration) rental_duration
   FROM film f
   JOIN film_category fc
     ON fc.film_id = f.film_id
   JOIN category c
     ON c.category_id = fc.category_id
- WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music');
+ WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+ GROUP BY 1 
+ ORDER BY 2 DESC;
