cab945b08cdef041819b6a27dbbeb03b93f7a73d
ronin62715sv <slateralexander219@yahoo.com>
2024-10-08 21:51:58 -0400

    Refactored query 3 to sort quartiles by column.

--- Q3.sql
+++ Q3.sql
@@ -4,17 +4,18 @@ WITH t1
   AS (SELECT f.title film_title,
              c.name category_name,
              f.rental_duration rental_duration,
-             NTILE(4) OVER (ORDER BY rental_duration) AS standard_quartile FROM film f
+             NTILE(4) OVER (ORDER BY rental_duration) AS standard_quartile 
+        FROM film f
         JOIN film_category fc
-        ON fc.film_id = f.film_id
+          ON fc.film_id = f.film_id
         JOIN category c
-        ON c.category_id = fc.category_id WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+          ON c.category_id = fc.category_id WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
        ORDER BY 4)
 SELECT category_name,
-       standard_quartile,
-       COUNT(film_title)
+       COUNT(CASE WHEN standard_quartile = 1 THEN film_title END) AS first_quartile, 
+       COUNT(CASE WHEN standard_quartile = 2 THEN film_title END) AS second_quartile,
+       COUNT(CASE WHEN standard_quartile = 3 THEN film_title END) AS third_quartile,
+       COUNT(CASE WHEN standard_quartile = 4 THEN film_title END) AS fourth_quartile
   FROM t1
-GROUP BY 1,
-         2
- ORDER BY 1,
-          2;
+ GROUP BY 1
+ ORDER BY 1;
